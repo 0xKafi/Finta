284a308bb0e589e771f28c764aa908eeeebd0096
Kafi <ahkafi05@gmail.com>
2026-02-04 02:13:07 +0600

    Add vertical line

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div className='bg-linear-to-b from-[#f5faff] to-[#bdd8fc] px-4'>
         <div className='max-w-4xl mx-auto h-screen relative px-4'>
+          <div className='absolute w-px left-0 inset-y-0 h-full bg-neutral-300'></div>
+          <div className='absolute w-px right-0 inset-y-0 h-full bg-neutral-300'></div>
             <Navbar></Navbar>
         </div>
     </div>
